c6b0ce5d0a69b4b79439b4e53eaf41c39e34fdcc
POLA <ragadeep.pola@capgemini.com>
2019-02-22 13:59:24 +0530

    Adds BookMyMovie project

---
+++ movie-website/src/main/java/com/cg/bookmymovie/websiteService/WebsiteServiceApplication.java
@@ -0,0 +1,30 @@
+package com.cg.bookmymovie.websiteService;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WebsiteServiceApplication /* extends SpringBootServletInitializer */ {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteServiceApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+
+	
+	
+
+}
+
